b43f1135ad2760e848a2de741d1fd98a41d580f3
EasternSPAWN <zx174@hotmail.com>
2025-05-28 20:38:36 +0100

    first import

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-# function to return book from file
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
+from stats import get_num_words
+
+def get_book_text(path):
+    with open(path) as f:
         return f.read()
-# function to call contents of file
+
 def main():
     text = get_book_text("books/frankenstein.txt")
-    print(text)
-# execute
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
 main()
