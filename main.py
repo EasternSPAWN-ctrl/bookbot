10423ac049063998fdd1cb39bf4a9cb0ccaf100e
EasternSPAWN <zx174@hotmail.com>
2025-05-30 19:02:24 +0100

    completed bookbot for test

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-from stats import get_num_words
+import sys
+from stats import get_num_words, get_char_counts, sort_char_dict
 
-def get_book_text(path):
-    with open(path) as f:
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
         return f.read()
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    # Check if the correct number of arguments is passed
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+
+    text = get_book_text(path)
+
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+
+    char_counts = get_char_counts(text)
+    sorted_chars = sort_char_dict(char_counts)
+
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,28 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_char_counts(text):
+    char_counts = {}
+
+    for char in text.lower():
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+
+    return char_counts
+
+def sort_on(d):
+    return d["num"]
+
+def sort_char_dict(char_dict):
+    sorted_list = []
+
+    for char, count in char_dict.items():
+        if char.isalpha():
+            sorted_list.append({"char": char, "num": count})
+
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
